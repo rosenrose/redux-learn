3914df8eae266c9ffb51dd0b84c9481cff1c2551
rosenrose <crazytempler@gmail.com>
2022-04-04 16:40:53 +0900

    pure redux: counter

--- src/index.js
+++ src/index.js
@@ -7,3 +7,31 @@
 //     <App />
 //   </React.StrictMode>
 // );
+import { createStore } from "redux";
+
+const [minus, plus] = document.querySelectorAll("button");
+const number = document.querySelector("span");
+const ADD = "add";
+const SUB = "subtract";
+
+const store = createStore((state = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return state + 1;
+    case SUB:
+      return state - 1;
+    default:
+      return state;
+  }
+});
+store.subscribe(() => {
+  number.textContent = store.getState();
+});
+
+store.dispatch({ type: "" });
+plus.addEventListener("click", () => {
+  store.dispatch({ type: ADD });
+});
+minus.addEventListener("click", () => {
+  store.dispatch({ type: SUB });
+});
