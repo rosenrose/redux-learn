00cc322c3c999b50400975d8350394f0fa55f282
rosenrose <crazytempler@gmail.com>
2022-04-04 17:37:39 +0900

    pure redux: todo

--- src/index.js
+++ src/index.js
@@ -7,14 +7,17 @@
 //     <App />
 //   </React.StrictMode>
 // );
+
 import { createStore } from "redux";
+
+// Counter
 
 const [minus, plus] = document.querySelectorAll("button");
 const number = document.querySelector("span");
 const ADD = "add";
 const SUB = "subtract";
 
-const store = createStore((state = 0, action) => {
+const countStore = createStore((state = 0, action) => {
   switch (action.type) {
     case ADD:
       return state + 1;
@@ -24,14 +27,64 @@ const store = createStore((state = 0, action) => {
       return state;
   }
 });
-store.subscribe(() => {
-  number.textContent = store.getState();
+countStore.subscribe(() => {
+  number.textContent = countStore.getState();
 });
 
-store.dispatch({ type: "" });
+countStore.dispatch({ type: "" });
 plus.addEventListener("click", () => {
-  store.dispatch({ type: ADD });
+  countStore.dispatch({ type: ADD });
 });
 minus.addEventListener("click", () => {
-  store.dispatch({ type: SUB });
+  countStore.dispatch({ type: SUB });
+});
+
+// To do
+
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
+
+const todoStore = createStore((state = [], action) => {
+  const { type, text, id } = action;
+
+  switch (type) {
+    case ADD:
+      return [{ text, id }, ...state];
+    case SUB:
+      return state.filter((todo) => todo.id !== id);
+    default:
+      return state;
+  }
+});
+const addTodo = (text) => {
+  return { type: ADD, text, id: Date.now() };
+};
+const deleteTodo = (id) => {
+  return { type: SUB, id };
+};
+
+const createTodo = (todo) => {
+  const template = document.querySelector("#todoTemplate").content.cloneNode(true);
+  const li = template.firstElementChild;
+  li.id = todo.id;
+
+  template.querySelector("span").textContent = todo.text;
+  template.querySelector("button").addEventListener("click", (event) => {
+    todoStore.dispatch(deleteTodo(todo.id));
+  });
+  ul.append(li);
+};
+
+todoStore.subscribe(() => {
+  ul.replaceChildren();
+  // console.log(todoStore.getState());
+  todoStore.getState().forEach((todo) => {
+    createTodo(todo);
+  });
+});
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  todoStore.dispatch(addTodo(input.value));
+  input.value = "";
 });
